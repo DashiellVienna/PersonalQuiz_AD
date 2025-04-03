69e2678322e21192594a041e469a6fd10f3bde5d
Andrei <101584932+DashiellVienna@users.noreply.github.com>
2025-04-03 13:16:53 +0200

    Preparations of the project and First Screen set up.

---
+++ PersonalQuiz_AD/ViewControllers/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz_AD
+//
+//  Created by Andrei Deshin on 03.04.25.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
